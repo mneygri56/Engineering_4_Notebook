8e8e39c95f771e859822b2a6f096541ff9bdab67
mneygri56 <mneygri56@charlottesvilleschools.org>
2019-09-06 16:39:08 -0400

    actually blinks 10 times instead of 9

--- Python/GPIO Pins python.py
+++ Python/GPIO Pins python.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.OUT)
 GPIO.setup(12,GPIO.OUT)
 
-for i in range(1,10):
+for i in range(0,10):
     GPIO.output(12,1)
     GPIO.output(11,1)
     sleep(0.5)
