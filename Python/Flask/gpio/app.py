4e9768871976e3fc40b05185797a6043a91a969d
mneygri56 <mneygri56@charlottesvilleschools.org>
2019-09-10 12:09:17 -0400

    AAAAAAAAAAAAAAAAAAAAAAAH

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -4,22 +4,29 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
 
+blueState = False
+redState = False
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST", "NEWPOST"])
+@app.route("/", methods=["GET","POST"])
 def index():
-	if request.method == "On17":
-		GPIO.output(17,GPIO.HIGH)
-		msg = request.form.get("onBtn")
-	elif request.method == "Off17":
-		GPIO.output(17, GPIO.LOW)
-		msg = request.form.get("offBtn")
-	elif request.method == 
+	msg = "asfjojsghiojhiodfg"
+	if request.method == "POST":
+		if 'blue' in request.form:
+			GPIO.output(17,GPIO.HIGH)
+		elif 'red' in request.form:
+			GPIO.output(18,GPIO.HIGH)
 	else:
 		GPIO.output(17,GPIO.LOW)
 		msg = "No click yet."
 	return render_template("index.html", msg=msg)
 
+def blueOn():
+	GPIO.output(17,GPIO.HIGH)
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
+
+GPIO.cleanup()
